ba04cb7cadd23c5bb10e4f348788c4cd60118822
Anderson Barg <andbarg@yahoo.com.br>
2025-08-15 18:49:34 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/aula/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.aula.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aula.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
