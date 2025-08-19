7c9bb6ceb48d3f2c3242e5c48ab47313e07f56cc
Anderson Barg <andbarg@yahoo.com.br>
2025-08-19 15:59:24 -0300

    Get SQL script from local PostgreSQL

--- src/main/java/com/aula/course/entities/Product.java
+++ src/main/java/com/aula/course/entities/Product.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +30,7 @@ public class Product implements Serializable {
 	
 	private String name;
 	
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	
 	private Double price;
